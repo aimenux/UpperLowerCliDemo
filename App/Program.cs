5c5460f37cbd1271c95e9ba639790174ef4bc0bb
aimenux <devplusdev@gmail.com>
2020-11-17 09:59:19 +0100

    Fist commit !

---
+++ App/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public static class Program
+    {
+        public static Task Main(string[] args)
+        {
+            return CreateHostBuilder(args).RunCommandLineApplicationAsync<UpperLowerCommand>(args);
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddCommandLine(args);
+                    config.AddEnvironmentVariables();
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                })
+                .ConfigureLogging((hostingContext, loggingBuilder) =>
+                {
+                    loggingBuilder.AddConsoleLogger();
+                    loggingBuilder.AddNonGenericLogger();
+                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                })
+                .ConfigureServices((hostingContext, services) =>
+                {
+                    services.AddSingleton<UpperLowerCommand>();
+                });
+
+        private static void AddConsoleLogger(this ILoggingBuilder loggingBuilder)
+        {
+            loggingBuilder.AddConsole(options =>
+            {
+                options.DisableColors = false;
+                options.TimestampFormat = "[HH:mm:ss:fff] ";
+            });
+        }
+
+        private static void AddNonGenericLogger(this ILoggingBuilder loggingBuilder)
+        {
+            var services = loggingBuilder.Services;
+            services.AddSingleton(serviceProvider =>
+            {
+                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+                return loggerFactory.CreateLogger("SharpDocxDemo");
+            });
+        }
+    }
+}
---
+++ App/UpperLowerCommand.cs
@@ -0,0 +1,54 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace App
+{
+    [Command(Name = "UpperLowerCli", FullName = "UpperLower CLI", Description = "An UpperLower CLI Tool.")]
+    [VersionOptionFromMember(MemberName = nameof(GetVersion))]
+    [HelpOption]
+    public class UpperLowerCommand
+    {
+        [Required]
+        [Argument(0, "Text", "Some text to upperize or lowerize")]
+        public string Text { get; set; }
+
+        [Option("-u|--upper", "Convert the text to uper case.", CommandOptionType.NoValue)]
+        public bool UpperCase { get; set; }
+
+        [Option("-l|--lower", "Convert the text to lower case.", CommandOptionType.NoValue)]
+        public bool LowerCase { get; set; }
+
+        public void OnExecute(CommandLineApplication app)
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+            {
+                app.ShowHelp();
+                return;
+            }
+
+            if (UpperCase)
+            {
+                ConsoleColor.Magenta.WriteLine(Text.ToUpper());
+                return;
+            }
+                
+            if (LowerCase)
+            {
+                ConsoleColor.Yellow.WriteLine(Text.ToLower());
+                return;
+            }
+
+            ConsoleColor.Cyan.WriteLine(Text);
+        }
+
+        private static string GetVersion()
+        {
+            return typeof(UpperLowerCommand)
+                .Assembly
+                ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion;
+        }
+    }
+}
